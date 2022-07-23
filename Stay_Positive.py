b78a35ec15a0e0f6d85159292361450220f2dca1
SalomonDemma <dennasa@edu.hac.ac.il>
2022-07-23 20:44:58 +0300

    Stay_Positive is fixed

--- Stay_Positive.py
+++ Stay_Positive.py
@@ -1,14 +1,13 @@
-def get_positive_numbers() -> list:
+def get_positive_numbers(list_of_str_numbers: list) -> list:
     """
     A function that receives input from the user using input.
     User n will be asked to enter a series of numbers separated by a comma.
     The function will return all the positive numbers that the user entered,
     as a list of int numbers.
     """
-    inp_num = input('Enter a series of numbers separated by a comma:\n').split(',')
-    lst = list(map(lambda num: int(num), inp_num))
-    return [num for num in lst if num > 0]
+    list_of_int_numbers = list(map(lambda num: int(num), list_of_str_numbers))
+    return [num for num in list_of_int_numbers if num > 0]
 
 
 if __name__ == "__main__":
-    print(get_positive_numbers())
+    print(get_positive_numbers(input('Enter a series of numbers separated by a comma:\n').split(',')))
