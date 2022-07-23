1a3f4c966a5e35d915892ecba0b9af70fcf2fd82
SalomonDemma <dennasa@edu.hac.ac.il>
2022-07-23 23:17:15 +0300

    T_tent_P_its_python is fixed

--- T_tent_P_its_python.py
+++ T_tent_P_its_python.py
@@ -1,10 +1,11 @@
+import string
+
+
 def get_letters() -> list:
     """
     A function that returns the list of all characters between a and z and between A and Z.
     """
-    acb_list = list(map(lambda low_c: chr(low_c), range(ord('a'), ord('z') + 1)))  # adding the lower letter
-    acb_list += list(map(lambda low_c: chr(low_c), range(ord('A'), ord('Z') + 1)))  # adding the upper letter
-    return acb_list
+    return list(string.ascii_letters)
 
 
 if __name__ == "__main__":
